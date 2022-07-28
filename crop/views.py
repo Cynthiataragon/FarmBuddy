0d156bbe7e7359dd54f912157b1953a76d5b5455
Cynthiataragon <cynthiataragon@gmail.com>
2022-07-28 11:19:01 +0300

    A new project

---
+++ crop/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views import View
+
+from .models import CropModel
+
+
+# Create your views here.
+#  get all crops class view
+class CropView(View):
+    def get(self, request):
+        crops = CropModel.objects.all()
+        return render(request, 'crop.html', {'crops': crops})
+
+    def post(self, request):
+        # svae CropModel data
+        crop = CropModel(
+            name=request.POST['name'],
+            created_at=request.POST['created_at'],
+            updated_at=request.POST['updated_at'],
+        )
+        crop.save()
+        return render(request, 'crop.html', {'crops': CropModel.objects.all()})
---
+++ users/urls.py
@@ -0,0 +1,13 @@
+# api first django app
+from views import register
+
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('home/', home, name='hpme'),
+    path('login/', auth_views.LoginView.as_view(template_name=' users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+   
+]
