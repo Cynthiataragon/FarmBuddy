b12ec42cda612b601b6756b961f42eb8ec28543b
Cynthiataragon <cynthiataragon@gmail.com>
2022-07-29 07:59:29 +0300

    fixed the bug in login and registration

--- farmbuddy/urls.py
+++ farmbuddy/urls.py
@@ -19,13 +19,13 @@ from django.contrib.auth import views as auth_views
 from users import views as users_views
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
+from users import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', users_views.home, name='home'),   
-    path("login", views.login_request, name="login")
+    path("login", views.login_request, name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('api/v2/', include('crop.urls')),
     path("register", views.register_request, name="register")
